35ef8e2762eee4fce9cea3f20dc40f552898c9b9
Meghana S <87467546+meghana2903@users.noreply.github.com>
2021-07-18 12:34:31 +0530

    Add files via upload

---
+++ method2.java
@@ -0,0 +1,11 @@
+package introductiontoTestng;
+
+import org.testng.annotations.Test;
+
+class method2 {
+	@Test
+	public void demo1() {
+		System.out.println("hey this is METHOD 2");
+	}
+
+}
---
+++ method3.java
@@ -0,0 +1,18 @@
+package introductiontoTestng;
+
+import org.testng.annotations.Test;
+
+public class method3 {
+	@Test
+	public void demo1() {
+		System.out.println("hey this is METHOD 3");
+	}
+	@Test
+	public void demo2() {
+		System.out.println("hey this is METHOD 3");
+	}
+
+
+	
+
+}
