bb6be8c8ed64d7e648f48efb9eee22e9f81ac842
Meghana S <87467546+meghana2903@users.noreply.github.com>
2021-07-24 16:38:29 +0530

    Add files via upload

---
+++ ChromeDriver.java
@@ -0,0 +1,5 @@
+package withselenium;
+
+public class ChromeDriver {
+
+}
---
+++ LoginClass.java
@@ -0,0 +1,13 @@
+package withselenium;
+
+public class LoginClass {
+
+	static String url = "https://accounts.datoms.io/login";
+	static String emailxpath = "//*[@id='email']" ;
+	static String passxpath = "#password";	
+	static  String loginxpath = "#form_submit_btn";
+	static  String emailkeys = "ssmeghanass29@.com";
+	static  String passleys ="Datoms@123";
+
+
+}
